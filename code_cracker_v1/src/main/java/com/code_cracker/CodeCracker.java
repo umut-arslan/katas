accc635fbf60f4c5ce172a3782791eebb8e7bd43
Umut Arslan <umut.arslan@klosebrothers.de>
2023-01-06 10:50:47 +0100

    add and solve code_cracker_v1

---
+++ code_cracker_v1/src/main/java/com/code_cracker/CodeCracker.java
@@ -0,0 +1,58 @@
+package com.code_cracker;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Hello world!
+ *
+ */
+public class CodeCracker
+{
+    private final String alphabet;
+    private final String decryptionKey;
+    public CodeCracker(String alphabet, String decryptionKey) {
+        this.alphabet = alphabet;
+        this.decryptionKey = decryptionKey;
+    }
+    public int getPositionInAlphabet(final String letter) {
+        final String[] idx = alphabet.split(" ");
+        return Arrays.stream(idx).toList().indexOf(letter);
+    }
+    public int getPositionInDecryptionKey(final String letter) {
+        final String[] idx = decryptionKey.split(" ");
+        return Arrays.stream(idx).toList().indexOf(letter);
+    }
+
+    public String encrypt(final String word) {
+        return word.chars().mapToObj(i -> (char) i)
+                .map(String::valueOf)
+                .map(this::getPositionInAlphabet).map(this::getLetterForPosition)
+                .collect(Collectors.joining());
+    }
+
+    public String getLetterForPosition(final int i) {
+        final String[] idx = decryptionKey.split(" ");
+        if (i == -1) {
+            return " ";
+        }
+        return idx[i];
+    }
+
+    public String getDecryptionForPosition(final int i) {
+        final String[] idx = alphabet.split(" ");
+        if (i == -1) {
+            return " ";
+        }
+        return idx[i];
+    }
+
+    public String decrypt(final String word) {
+        return word.chars().mapToObj(i -> (char) i)
+                .map(String::valueOf)
+                .map(this::getPositionInDecryptionKey).map(this::getDecryptionForPosition)
+                .collect(Collectors.joining());
+    }
+}
