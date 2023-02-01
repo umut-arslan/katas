bd868bc95f5ead4b0ebbb7a095f5a5ef27fec6f8
Umut Arslan <umut.arslan@klosebrothers.de>
2023-02-01 12:28:12 +0100

    correct cardinal points

--- mars_rover/src/main/java/com/mars_rover/MarsRover.java
+++ mars_rover/src/main/java/com/mars_rover/MarsRover.java
@@ -5,10 +5,8 @@ import java.awt.*;
 
 /**
  * Hello world!
- *
  */
-public class MarsRover
-{
+public class MarsRover {
     private Point position;
     private String direction;
 
@@ -16,16 +14,18 @@ public class MarsRover
         this.position = new Point(position);
         this.direction = direction;
     }
+
     public static MarsRover create(Point startingPoint, String direction) {
         return new MarsRover(startingPoint, direction);
     }
 
     public void move(String move) {
-        if ((move == "f" && direction == "S") || (move == "b" && direction == "B")) position.x+=1;
-        else if ((move == "f" && direction == "W") || (move == "b" && direction == "S")) position.x-=1;
-        else if ((move == "f" && direction == "N") || (move == "b" && direction == "E")) position.y+=1;
-        else if ((move == "b" && direction == "N") || (move == "f" && direction == "E")) position.y-=1;
+        if ((move == "f" && direction == "E") || (move == "b" && direction == "W")) position.x += 1;
+        else if ((move == "f" && direction == "W") || (move == "b" && direction == "E")) position.x -= 1;
+        else if ((move == "f" && direction == "N") || (move == "b" && direction == "S")) position.y += 1;
+        else if ((move == "b" && direction == "N") || (move == "f" && direction == "S")) position.y -= 1;
     }
+
     public Point getPosition() {
         return position;
     }
