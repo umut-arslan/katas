008d9861b181484d998461aeaf392b47fef69982
Umut Arslan <umut.arslan@klosebrothers.de>
2023-02-01 11:18:17 +0100

    add getCurrentPosition test

--- mars_rover/src/main/java/com/mars_rover/MarsRover.java
+++ mars_rover/src/main/java/com/mars_rover/MarsRover.java
@@ -1,18 +1,25 @@
 package com.mars_rover;
 
+
+import java.awt.*;
+
 /**
  * Hello world!
  *
  */
 public class MarsRover
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    final Point startingPoint;
+    final String direction;
+
+    private MarsRover(Point startingPoint, String direction) {
+        this.startingPoint = new Point(startingPoint);
+        this.direction = direction;
     }
-
-    public static String test() {
-        return "hello";
-
+    public static MarsRover create(Point startingPoint, String direction) {
+        return new MarsRover(startingPoint, direction);
+    }
+    public Point getPosition() {
+        return startingPoint;
     }
 }
--- mars_rover/src/test/java/com/mars_rover/MarsRoverTest.java
+++ mars_rover/src/test/java/com/mars_rover/MarsRoverTest.java
@@ -2,13 +2,15 @@ package com.mars_rover;
 
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+
 import static org.assertj.core.api.Assertions.*;
 
 class MarsRoverTest{
-
     @Test
-    void testTest() {
-        assertThat(MarsRover.test()).isEqualTo("hello");
+    void getCurrentPosition() {
+        MarsRover rover = MarsRover.create(new Point(1, 1), "W");
+        assertThat(rover.getPosition()).isEqualTo(new Point(1,1));
     }
 
 }
