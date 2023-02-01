1d611ef34c433e090d47b681e7e8d45da4df1d1e
Umut Arslan <umut.arslan@klosebrothers.de>
2023-02-01 12:38:58 +0100

    add turnLeft test

--- mars_rover/src/test/java/com/mars_rover/MarsRoverTest.java
+++ mars_rover/src/test/java/com/mars_rover/MarsRoverTest.java
@@ -6,36 +6,44 @@ import java.awt.*;
 
 import static org.assertj.core.api.Assertions.*;
 
-class MarsRoverTest{
+class MarsRoverTest {
     @Test
     void getCurrentPosition() {
         MarsRover rover = MarsRover.create(new Point(1, 1), "W");
-        assertThat(rover.getPosition()).isEqualTo(new Point(1,1));
+        assertThat(rover.getPosition()).isEqualTo(new Point(1, 1));
     }
 
     @Test
     void moveForward() {
-        MarsRover rover = MarsRover.create(new Point(1, 1), "S");
+        MarsRover rover = MarsRover.create(new Point(1, 1), "E");
         rover.move("f");
-        assertThat(rover.getPosition()).isEqualTo(new Point(2,1));
+        assertThat(rover.getPosition()).isEqualTo(new Point(2, 1));
     }
 
     @Test
     void moveUp() {
         MarsRover rover = MarsRover.create(new Point(1, 1), "N");
         rover.move("f");
-        assertThat(rover.getPosition()).isEqualTo(new Point(1,2));
+        assertThat(rover.getPosition()).isEqualTo(new Point(1, 2));
     }
+
     @Test
     void moveDown() {
         MarsRover rover = MarsRover.create(new Point(1, 1), "N");
         rover.move("b");
-        assertThat(rover.getPosition()).isEqualTo(new Point(1,0));
+        assertThat(rover.getPosition()).isEqualTo(new Point(1, 0));
     }
+
     @Test
     void getDirection() {
         MarsRover rover = MarsRover.create(new Point(1, 1), "S");
-        rover.move("b");
         assertThat(rover.getDirection()).isEqualTo("S");
+    }
+
+    @Test
+    void turnLeft() {
+        MarsRover rover = MarsRover.create(new Point(1, 1), "S");
+        rover.turn("l");
+        assertThat(rover.getDirection()).isEqualTo("E");
     }
 }
