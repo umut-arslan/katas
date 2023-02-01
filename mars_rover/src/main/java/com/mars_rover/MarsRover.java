bac99b52cf1328bf5d96b9bd88d5a6fb021706c4
Umut Arslan <umut.arslan@klosebrothers.de>
2023-02-01 11:42:47 +0100

    add moveBackwards test

--- mars_rover/src/main/java/com/mars_rover/MarsRover.java
+++ mars_rover/src/main/java/com/mars_rover/MarsRover.java
@@ -9,17 +9,24 @@ import java.awt.*;
  */
 public class MarsRover
 {
-    final Point startingPoint;
-    final String direction;
+    private Point position;
+    private String direction;
 
-    private MarsRover(Point startingPoint, String direction) {
-        this.startingPoint = new Point(startingPoint);
+    private MarsRover(Point position, String direction) {
+        this.position = new Point(position);
         this.direction = direction;
     }
     public static MarsRover create(Point startingPoint, String direction) {
         return new MarsRover(startingPoint, direction);
     }
+
+    public void move(String move) {
+        if ((move == "f" && direction == "S") || (move == "b" && direction == "B")) position.x+=1;
+        else if ((move == "f" && direction == "W") || (move == "b" && direction == "S")) position.x-=1;
+        else if ((move == "f" && direction == "N") || (move == "b" && direction == "E")) position.y+=1;
+        else if ((move == "b" && direction == "N") || (move == "f" && direction == "E")) position.y-=1;
+    }
     public Point getPosition() {
-        return startingPoint;
+        return position;
     }
 }
--- mars_rover/src/test/java/com/mars_rover/MarsRoverTest.java
+++ mars_rover/src/test/java/com/mars_rover/MarsRoverTest.java
@@ -13,4 +13,17 @@ class MarsRoverTest{
         assertThat(rover.getPosition()).isEqualTo(new Point(1,1));
     }
 
+    @Test
+    void moveForward() {
+        MarsRover rover = MarsRover.create(new Point(1, 1), "S");
+        rover.move("f");
+        assertThat(rover.getPosition()).isEqualTo(new Point(2,1));
+    }
+
+    @Test
+    void moveBackwards() {
+        MarsRover rover = MarsRover.create(new Point(1, 1), "S");
+        rover.move("b");
+        assertThat(rover.getPosition()).isEqualTo(new Point(0,1));
+    }
 }
