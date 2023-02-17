38b01167299c4583caa198d8cabba03b93992d7a
Umut Arslan <umut.arslan@klosebrothers.de>
2023-02-17 12:20:20 +0100

    add getNewPosition function

--- mars_rover_v1/src/main/java/com/mars_rover/MarsRover.java
+++ mars_rover_v1/src/main/java/com/mars_rover/MarsRover.java
@@ -34,21 +34,41 @@ public class MarsRover {
         return new MarsRover(startingPoint, direction, obstacles);
     }
 
-    public void moveCheck(Point point) {
-        if (!obstacles.contains(point)) {
-            this.position = point;
-        }
+    public boolean moveCheck(Point point) {
+        return !obstacles.contains(point);
     }
 
-    public void move(String move) {
-        if ((move == "f" && direction == "E") || (move == "b" && direction == "W"))
-            moveCheck(new Point(this.position.x + 1, this.position.y));
-        else if ((move == "f" && direction == "W") || (move == "b" && direction == "E"))
-            moveCheck(new Point(this.position.x - 1, this.position.y));
-        else if ((move == "f" && direction == "N") || (move == "b" && direction == "S"))
-            moveCheck(new Point(this.position.x, this.position.y + 1));
-        else if ((move == "b" && direction == "N") || (move == "f" && direction == "S"))
-            moveCheck(new Point(this.position.x, this.position.y - 1));
+    public MarsRover move(String move) {
+        return MarsRover.create(new Point(getNewPosition(move)), this.direction, this.obstacles);
+//        if ((move == "f" && direction == "E") || (move == "b" && direction == "W"))
+//            moveCheck(new Point(this.position.x + 1, this.position.y));
+//        else if ((move == "f" && direction == "W") || (move == "b" && direction == "E"))
+//            moveCheck(new Point(this.position.x - 1, this.position.y));
+//        else if ((move == "f" && direction == "N") || (move == "b" && direction == "S"))
+//            moveCheck(new Point(this.position.x, this.position.y + 1));
+//        else if ((move == "b" && direction == "N") || (move == "f" && direction == "S"))
+//            moveCheck(new Point(this.position.x, this.position.y - 1));
+    }
+
+    private Point getNewPosition(String move) {
+        return move.chars().mapToObj(i -> {
+                    int x = this.position.x, y = this.position.y;
+
+                    if (i == 'f' && this.getDirection().equals("E") || i == 'b' && this.getDirection().equals("W"))
+                        x += 1;
+
+                    else if (i == 'f' && this.getDirection().equals("W") || i == 'b' && this.getDirection().equals("E"))
+                        x -= 1;
+
+                    else if (i == 'f' && this.getDirection().equals("N") || i == 'b' && this.getDirection().equals("S"))
+                        y += 1;
+
+                    else if (i == 'f' && this.getDirection().equals("S") || i == 'b' && this.getDirection().equals("N"))
+                        y -= 1;
+
+                    return new Point(x, y);
+                })
+                .reduce(new Point(0, 0), (p1, p2) -> new Point(p1.x + p2.x, p1.y + p2.y));
     }
 
     public Point getPosition() {
